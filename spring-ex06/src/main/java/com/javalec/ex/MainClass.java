512ca2e92f7670daf2bd2244c58e1b64281a93d0
dydwlsdlz <dydwlsdlz0527@gmail.com>
2018-11-06 23:48:41 +0900

    JAVA파일에 XML파일 포함 (DI설정)

---
+++ spring-ex06/src/main/java/com/javalec/ex/MainClass.java
@@ -0,0 +1,28 @@
+package com.javalec.ex;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		
+		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(ApplicationConfig.class);
+
+		Student student1 = ctx.getBean("student1",Student.class);
+		System.out.println(student1.getName());
+		System.out.println(student1.getAge());
+		System.out.println(student1.getHobbys());
+		System.out.println(student1.getHeight());
+		
+		Student student2 = ctx.getBean("student2",Student.class);
+		System.out.println(student2.getName());
+		System.out.println(student2.getAge());
+		System.out.println(student2.getHobbys());
+		System.out.println(student1.getHeight());
+		
+		ctx.close();
+	}
+
+}
